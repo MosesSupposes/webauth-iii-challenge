f57f32c1224624acd08a10abb2e0732a52378b4b
Moses Samuel <mosamuel98@gmail.com>
2019-10-16 17:03:29 -0700

    add 404 middleware and an error handling middleware  to the server

--- api/server.js
+++ api/server.js
@@ -10,5 +10,19 @@ server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
+server.use(function notFound(req, res, next) {
+    const error = new Error('Not found.')
+    error.status = 404
+
+    next(error)
+})
+
+server.use(function errorHandler(error, req, res, next) {
+    error.status = error.status || 500
+    error.message = error.message || 'Internal server error'
+
+    res.status(status).json({error: { errorMsg } })
+})
+
 
 server.listen(PORT, () => { console.log('server listenting on port ' + PORT) })
